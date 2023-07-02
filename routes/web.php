c640116b8df6a11756885c32f142e0ec9eeb3002
soenghakk3301 <soenghakkchoeurn3301@gmail.com>
2023-07-02 10:24:29 +0700

    laravel_contact_app

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts.index');
+})->name('contacts.index');
+Route::get('/contacts/create', function () {
+    return "<h1>Add new contact</h1>";
+})->name('contacts.create');
+Route::get('/contacts/{id}', function ($id) {
+    return "Contact " . $id;
+})->name('contacts.show');
